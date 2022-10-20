acc1a0cd1abe3e237cf8b25a30c050349d37a593
franchu <franchucersosimo@gmail.com>
2022-10-20 00:13:18 -0300

    add navbar efect

--- assets/main.js
+++ assets/main.js
@@ -2,11 +2,12 @@ const nav = document.querySelector(".nav")
 let lastScrollY = window.screenY;
 
 window.addEventListener("scroll", () => {
-    if(lastScrollY < window.scrollY){
+    if(lastScrollY < window.scrollY && window.scrollY > 934){
         nav.classList.add("hide_nav");
     } else {
         nav.classList.remove("hide_nav");
     }
     lastScrollY = window.scrollY;
+    console.log(window.scrollY)
 })
 
